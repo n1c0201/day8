156b8001d4440bada9a98ab97517a8b0531a595c
n1c0201 <56902710+n1c0201@users.noreply.github.com>
2019-12-02 16:45:50 +0700

    Question no 2

---
+++ prices.py
@@ -0,0 +1,6 @@
+prices = {"banana": 4, "apple": 2, "orange": 1.5, "pear": 3}
+stock = {"banana": 7, "apple": 0, "orange": 2, "pear": 4}
+for i in prices:
+    print(i)
+    print("price: " + str(prices[i]))
+    print("stock: " + str(stock[i]))
